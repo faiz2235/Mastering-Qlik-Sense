4a03bf2c1db395ed91329b948a79ee64a8569108
faiz2235 <47683920+faiz2235@users.noreply.github.com>
2024-05-25 02:58:00 -0400

    Adding new file now check.js

---
+++ Test/check.js
@@ -0,0 +1,8 @@
+//write console message to check the number
+console.log(1);
+
+//write console message to check the number
+console.log(2);
+
+//write console message to check the number
+console.log(3)
